20f6d58172b9528ff28935258859f18fe736e0fc
hassight <sisalem.hassan@gmail.com>
2021-01-17 20:38:05 +0100

    Added COO libraries.

--- dust/src/business/spring/SpringIoC.java
+++ dust/src/business/spring/SpringIoC.java
@@ -1,5 +1,8 @@
 package business.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringIoC {
 	private static final ApplicationContext context = new ClassPathXmlApplicationContext("business/spring/spring.xml");
 	
