253078614fe9ce47aa62a25f092000f77ccd600e
hassight <sisalem.hassan@gmail.com>
2021-01-17 23:10:30 +0100

    Started Lucene part.

--- dust/src/business/Test.java
+++ dust/src/business/Test.java
@@ -1,5 +1,12 @@
 package business;
 
-public class Test {
+import business.data.Coordinates;
+import business.data.Distance;
 
+public class Test {
+	public static void main(String[] args) {
+		Coordinates c = new Coordinates(-17.539757944124336, -149.56728263802216);
+		Coordinates c1 = new Coordinates(-17.532535360396363, -149.54468602215124);
+		System.out.println(Distance.getKMDistance(c, c1));
+	}
 }
---
+++ dust/src/persistence/lucene/LuceneConstants.java
@@ -0,0 +1,10 @@
+package persistence.lucene;
+
+public class LuceneConstants {
+	public static final String DATA_DIRECTORY_FIELD_NAME = "dataDirectoryPath";
+	public static final String INDEX_DIRECTORY_FIELD_NAME = "indexDirectoryPath";
+	public static final String MAX_SEARCH_NUMBER_FIELD_NAME = "maxSearch";
+	public static final String INDEX_FIRST_FIELD_FIELD_NAME = "firstFieldName";
+	public static final String INDEX_SECOND_FIELD_FIELD_NAME = "secondFieldName";
+	public static final String INDEX_THIRD_FIELD_FIELD_NAME = "thirdFieldName";
+}
